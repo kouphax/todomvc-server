9e9d132cbd38d7be08520dfa6d51566479fbdeba
Christian Horsdal <c.horsdal@gmail.com>
2013-04-02 23:50:34 +0200

    Added TodoMVC module, that serves the index page.

---
+++ examples/nancy/TodoMVCModule.cs
@@ -0,0 +1,12 @@
+﻿namespace TodoMVC.Server
+{
+  using Nancy;
+
+  public class TodoMVCModule : NancyModule
+  {
+    public TodoMVCModule()
+    {
+      Get["/index"] = _ => View["Content/index.htm"];
+    }
+  }
+}
