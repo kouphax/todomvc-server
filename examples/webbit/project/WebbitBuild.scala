843d9de261c96cb5d4fb950cde79e2a15d59dadc
James Hughes <james@yobriefca.se>
2013-04-24 19:45:44 +0100

    Added newer webbit version - still unpublished

--- examples/webbit/project/WebbitBuild.scala
+++ examples/webbit/project/WebbitBuild.scala
@@ -5,7 +5,7 @@ object WebbitBuild extends Build {
 
   lazy val webbit = Project("webbit", file(".")).settings(
     libraryDependencies ++= Seq(
-      "org.webbitserver"         % "webbit"             % "0.4.14",
+      "org.webbitserver"         % "webbit"             % "0.4.15-SNAPSHOT",
       "org.codehaus.jackson"     % "jackson-core-asl"   % "1.9.12",
       "org.codehaus.jackson"     % "jackson-mapper-asl" % "1.9.12",
       "com.google.guava"         % "guava"              % "14.0.1",
