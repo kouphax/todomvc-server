2413ff1299fb581b82c6d70eab4e54417ec67865
kubamarchwicki <kuba@marchwicki.pl>
2013-05-29 22:32:04 +0200

    Silex DI container to wire dependencies and allow testing

--- examples/silex-mongodb/src/app.php
+++ examples/silex-mongodb/src/app.php
@@ -2,43 +2,49 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 
-//http://cambridgesoftware.co.uk/blog/item/59-backbonejs-%20-php-with-silex-microframework-%20-mysql
-
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use TodoMVC\Model\Todo;
-
-$store = new TodoMVC\Data\Store(new \MongoClient());
+use TodoMVC\Data\Store;
 
 $app = new Silex\Application();
 $app['debug'] = true;
 
-$app->get('/todos', function() use ($app, $store) {
+$app['mongo'] = $app->share(function () {
+    return new \MongoClient();
+});
+
+$app['store'] = $app->share(function ($app) {
+    return new Store($app['mongo']);
+});
+
+$app->get('/todos', function() use ($app) {
+    $store = $app['store'];
     return $app->json($store->getAll());
 });
 
-$app->get('/todos/{id}', function($id) use ($app, $store) {
+$app->get('/todos/{id}', function($id) use ($app) {
+    $store = $app['store'];
     return $app->json($store->get($id));
 });
 
-$app->post('/todos', function(Request $request) use ($app, $store) {
+$app->post('/todos', function(Request $request) use ($app) {
+    $store = $app['store'];
     $data = json_decode($request->getContent());
-//    $todo = new Todo($data->title, $data->order, $data->completed);
-    $todo = $store->create($data);
 
+    $todo = $store->create($data);
     return $app->json($todo, 201);
 });
 
-$app->put('/todos/{id}', function($id, Request $request) use ($app, $store) {
+$app->put('/todos/{id}', function($id, Request $request) use ($app) {
+    $store = $app['store'];
     $data = json_decode($request->getContent());
-//    $todo = new Todo($data->title, $data->order, $data->completed);
-//    $todo->id = $data->id;
 
     $store->update($id, $data);
     return new Response('updated', 204);
 });
 
-$app->delete('/todos/{id}', function($id) use ($app, $store) {
+$app->delete('/todos/{id}', function($id) use ($app) {
+    $store = $app['store'];
     $store->remove($id);
     return new Response('deleted', 200);
 });
